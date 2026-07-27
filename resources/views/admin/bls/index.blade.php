38ddbbda089d3d75e2f52bc3af1dbeba608c6a71
SEYDOU Zaritatou <seydouzaria2@gmail.com>
2026-07-27 16:04:17 +0000

    Inverser l'ordre ETA / date reception doc dans le tableau BLS
    La colonne DATE RECEPTION DOC doit venir avant ETA, comme demande.

--- resources/views/admin/bls/index.blade.php
+++ resources/views/admin/bls/index.blade.php
@@ -24,8 +24,8 @@
                             <th class="text-center align-middle">COMPAGNIE</th>
                             <th class="text-center align-middle">TYPE</th>
                             <th class="text-center align-middle">CONTENEURS</th>
-                            <th class="text-center align-middle">ETA</th>
                             <th class="text-center align-middle">DATE RÉCEPTION<br>DOC</th>
+                            <th class="text-center align-middle">ETA</th>
                             <th class="text-center align-middle" style="max-width: 160px;">DESCRIPTION<br>MARCHANDISE</th>
                             <th class="text-center align-middle">ECHANGE<br>BL</th>
                             <th class="text-center align-middle">RÉCEPTION<br>BAD</th>
@@ -57,8 +57,8 @@
                     {data: (d) => d.company_name ?? '-'},
                     {data: (d) => d.type_operation ?? '-'},
                     {data: (d) => (d.container_labels && d.container_labels.length) ? d.container_labels.join('<br>') : '-'},
-                    {data: (d) => d.eta_date ? moment(d.eta_date).format('DD/MM/YYYY') : '-'},
                     {data: (d) => d.created ? moment(d.created).format('DD/MM/YYYY') : '-'},
+                    {data: (d) => d.eta_date ? moment(d.eta_date).format('DD/MM/YYYY') : '-'},
                     {data: (d) => `<div style="max-width: 160px; white-space: normal; word-break: break-word;">${d.description ?? '-'}</div>`},
                     {data: (d) => d.exchange_date ? moment(d.exchange_date).format('DD/MM/YYYY') : '-'},
                     {data: (d) => d.bad_date ? moment(d.bad_date).format('DD/MM/YYYY') : '-'},
